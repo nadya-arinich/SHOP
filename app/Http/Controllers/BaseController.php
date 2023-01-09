bc0cbe77799f0dde723aa4700a3bcabf62a2d29d
nadya-arinich <97697219+nadya-arinich@users.noreply.github.com>
2023-01-09 20:48:59 +0300

    Add files via upload

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{
+    public function getIndex() {
+       return view('welcome');
+    }
+
+    public function about() {
+        return view('about');
+    }
+
+
+}
